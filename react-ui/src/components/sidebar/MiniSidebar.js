23559342ea216737eeaad07de9810d31874004b9
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2023-03-26 23:47:23 +0530

    Add Border-boxes

--- react-ui/src/components/sidebar/MiniSidebar.js
+++ react-ui/src/components/sidebar/MiniSidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AddIcon } from '@chakra-ui/icons';
 import ShopeeLogo from '../../assets/img/MiniSidebar/Shopee_logo.svg';
 import lazada from '../../assets/img/MiniSidebar/lazada.png';
@@ -9,78 +9,205 @@ import tokopedia from '../../assets/img/MiniSidebar/tokopedia.svg';
 import bukalapak from '../../assets/img/MiniSidebar/bukalapak.png';
 import tiki from '../../assets/img/MiniSidebar/tiki.jpeg';
 
-
-
 import { Image } from '@chakra-ui/react';
-import { Box, Icon, VStack, useColorModeValue, Tooltip} from '@chakra-ui/react';
+import { Box, Icon, VStack, useColorModeValue, Tooltip } from '@chakra-ui/react';
 import { AiFillHome, AiOutlineSetting } from 'react-icons/ai';
 
 const MiniSidebar = () => {
   const backgroundColor = useColorModeValue('white', 'navy.800');
   const iconColor = useColorModeValue('gray.600', 'white');
+  const [selectedIcon, setSelectedIcon] = useState(null);
+
+  const handleIconClick = (iconName) => {
+    setSelectedIcon(iconName);
+  };
+
+  const isSelected = (iconName) => {
+    return iconName === selectedIcon;
+  };
 
   return (
     <Box
-    bg={backgroundColor}
-    borderRadius='5px'
-    borderRight='1px solid #4A5568'
-    w='60px'
-    h='100vh'
-    position='fixed'
-    display={{ sm: 'none', xl: 'block' }}
-  >
-    <VStack
-      alignItems="center"
-      justifyContent="flex-start" // Change this line
-      spacing={8}
-      h="100%"
-      pt="1.5rem" // Add this line to add some margin at the top
+      bg={backgroundColor}
+      borderRadius='5px'
+      borderRight='1px solid #4A5568'
+      w='60px'
+      h='100vh'
+      position='fixed'
+      display={{ sm: 'none', xl: 'block' }}
     >
+      <VStack
+        alignItems="center"
+        justifyContent="flex-start"
+        spacing={8}
+        h="100%"
+        pt="1.5rem"
+      >
+        <Tooltip label='Add New Platform' placement='right' hasArrow>
+          <Icon
+            as={AddIcon}
+            color={iconColor}
+            boxSize={6}
+            _hover={{ boxSize: 8, cursor: 'pointer' }}
+          />
+        </Tooltip>
 
-    <Tooltip label='Add New Platform' placement='right' hasArrow>
-    <Icon
-          as={AddIcon}
-          color={iconColor}
-          boxSize={6}
-          _hover={{ boxSize: 8, cursor: 'pointer' }}
-        />
-    </Tooltip>
+        {/* Shopee */}
+        <Tooltip label='Shopee' placement='right' hasArrow>
+          <Box
+            borderWidth={isSelected('Shopee') ? '3px' : '0px'}
+            borderRadius='5px'
+            borderColor='#68D391'
+            my="0.5rem"
+            onClick={() => handleIconClick('Shopee')}
+            p={isSelected('Shopee') ? '0.25rem' : '0px'} // Add padding when selected
+          >
+            <Image
+              src={ShopeeLogo}
+              boxSize="2rem"
+              _hover={isSelected('Shopee') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }} // Remove hover effect when selected
 
-    <Tooltip label='Shopee' placement='right' hasArrow>
-    <Image src={ShopeeLogo} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" /> 
-    </Tooltip>
-    <Tooltip label='Amazon' placement='right' hasArrow>
-    <Image src={amazonlogo} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip>
+            />
+          </Box>
+        </Tooltip>
+
+        {/* Amazon */}
+        <Tooltip label='Amazon' placement='right' hasArrow>
+          <Box
+            borderWidth={isSelected('Amazon') ? '3px' : '0px'}
+            borderRadius='5px'
+            borderColor='#68D391'
+            my="0.5rem"
+            onClick={() => handleIconClick('Amazon')}
+            p={isSelected('Amazon') ? '0.25rem' : '0px'}
+          >
+            <Image
+              src={amazonlogo}
+              boxSize="2rem"
+              _hover={isSelected('Amazon') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }} // Remove hover effect when selected
+              />
+              </Box>
+              </Tooltip>
+
+            {/* Instacart */}
     <Tooltip label='Instacart' placement='right' hasArrow>
-    <Image src={instacart} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip>
-    <Tooltip label='Walmart' placement='right' hasArrow>           
-    <Image src={walmart} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip>
-    <Tooltip label='Lazada' placement='right' hasArrow>
-    <Image src={lazada} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" /> 
-    </Tooltip>  
-    <Tooltip label='Tokopedia' placement='right' hasArrow>   
-    <Image src={tokopedia} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip>
-    <Tooltip label='Bukalapak' placement='right' hasArrow>   
-    <Image src={bukalapak} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip> 
-    <Tooltip label='Tiki' placement='right' hasArrow>   
-    <Image src={tiki} boxSize="2rem" _hover={{ transform: 'scale(1.8)', cursor: 'pointer' }} mt="1rem" />
-    </Tooltip> 
-    
-        <Icon
-          as={AiOutlineSetting}
-          color={iconColor}
-          boxSize={6}
-          _hover={{ boxSize: 8, cursor: 'pointer' }}
+      <Box
+        borderWidth={isSelected('Instacart') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Instacart')}
+        p={isSelected('Instacart') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={instacart}
+          boxSize="2rem"
+          _hover={isSelected('Instacart') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
         />
-        {/* Add more icons as needed */}
-      </VStack>
+      </Box>
+    </Tooltip>
+
+    {/* Walmart */}
+    <Tooltip label='Walmart' placement='right' hasArrow>
+      <Box
+        borderWidth={isSelected('Walmart') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Walmart')}
+        p={isSelected('Walmart') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={walmart}
+          boxSize="2rem"
+          _hover={isSelected('Walmart') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
+        />
+      </Box>
+    </Tooltip>
+
+    {/* Lazada */}
+    <Tooltip label='Lazada' placement='right' hasArrow>
+      <Box
+        borderWidth={isSelected('Lazada') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Lazada')}
+        p={isSelected('Lazada') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={lazada}
+          boxSize="2rem"
+          _hover={isSelected('Lazada') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
+        />
+      </Box>
+    </Tooltip>
+
+    {/* Tokopedia */}
+    <Tooltip label='Tokopedia' placement='right' hasArrow>
+      <Box
+        borderWidth={isSelected('Tokopedia') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Tokopedia')}
+        p={isSelected('Tokopedia') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={tokopedia}
+          boxSize="2rem"
+          _hover={isSelected('Tokopedia') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
+        />
+      </Box>
+    </Tooltip>
+
+    {/* Bukalapak */}
+    <Tooltip label='Bukalapak' placement='right' hasArrow>
+      <Box
+        borderWidth={isSelected('Bukalapak') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Bukalapak')}
+        p={isSelected('Bukalapak') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={bukalapak}
+          boxSize="2rem"
+          _hover={isSelected('Bukalapak') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
+          />
+          </Box>
+          </Tooltip>
+
+        {/* Tiki */}
+        <Tooltip label='Tiki' placement='right' hasArrow>
+      <Box
+        borderWidth={isSelected('Tiki') ? '3px' : '0px'}
+        borderRadius='5px'
+        borderColor='#68D391'
+        my="0.5rem"
+        onClick={() => handleIconClick('Tiki')}
+        p={isSelected('Tiki') ? '0.25rem' : '0px'}
+      >
+        <Image
+          src={tiki}
+          boxSize="2rem"
+          _hover={isSelected('Tiki') ? {} : { transform: 'scale(1.8)', cursor: 'pointer' }}
+        />
+      </Box>
+    </Tooltip>
+
+    <Icon
+      as={AiOutlineSetting}
+      color={iconColor}
+      boxSize={6}
+      _hover={{ boxSize: 8, cursor: 'pointer' }}
+    />
+    </VStack>
     </Box>
-  );
+    
+    );
 };
 
 export default MiniSidebar;
+      
